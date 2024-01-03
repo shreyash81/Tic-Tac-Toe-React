5db545e9e2cf05f34d581136ceaf3ad9a920fa1e
Tanish Mishra <tanishmishra97@gmail.com>
2024-01-03 13:58:49 +0530

    feat: mobile responsiveness added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,50 +6,54 @@ import Game from "../../components/GameArea/Game";
 import styles from "./Home.module.css";
 const Home = () => {
   let [player, setPlayer] = useState("");
-   
+
   let [page, setPage] = useState(1);
-     
-// let [localStore,setLocalStore]=useState(0);  
- 
+
+  // let [localStore,setLocalStore]=useState(0);
+
   useEffect(() => {
-   
-    
+    if (
+      localStorage.getItem("userScore") > 0 ||
+      localStorage.getItem("cpuScore") > 0 ||
+      localStorage.getItem("tiesScore") > 0
+    ) {
+      setPage(2);
+    }
 
-     if(localStorage.getItem('userScore') > 0 || localStorage.getItem('cpuScore') > 0 || localStorage.getItem('tiesScore') > 0) {
-       setPage(2);
-     }
-   
-  //  (Math.floor(Math.random()*2) ===1 ? setPlayer('big_x.svg') : setPlayer('big_zero.svg')) 
-   if(localStorage.getItem('players') != null) {
-    //  localStorage.setItem('players',player);
-    setPlayer(localStorage.getItem('players'));
-   }
-  //  (localStorage.getItem('players') == null ? setPlayer('big_x.svg') : setPlayer('big_zero.svg'))
-     
-  },[])
-
+    //  (Math.floor(Math.random()*2) ===1 ? setPlayer('big_x.svg') : setPlayer('big_zero.svg'))
+    if (localStorage.getItem("players") != null) {
+      //  localStorage.setItem('players',player);
+      setPlayer(localStorage.getItem("players"));
+    }
+    //  (localStorage.getItem('players') == null ? setPlayer('big_x.svg') : setPlayer('big_zero.svg'))
+  }, []);
 
   return (
-
-    <div className={styles.home}>
+    <>
+      <div className={styles.home}>
+        <div className={styles.game_container}>
+          {page === 1 && (
+            <MainMenu
+              player={player}
+              setPlayer={setPlayer}
+              page={page}
+              setPage={setPage}
+            />
+          )}
+          {page === 2 && (
+            <Game
+              player={player}
+              setPlayer={setPlayer}
+              setPage={setPage}
+              page={page}
+            />
+          )}
+        </div>
+      </div>
       <div className={styles.quote_container}>
         <Quote />
       </div>
-      <div className={styles.game_container}>
-        {page===1 && (
-          <MainMenu
-          player={player}
-          setPlayer={setPlayer}
-          page={page}
-          setPage={setPage}
-          />
-          )}
-        {page === 2 && (
-          <Game player={player} setPlayer={setPlayer} setPage={setPage} page={page}  />
-          )}
-      </div>
-    </div>
-
+    </>
   );
 };
 
